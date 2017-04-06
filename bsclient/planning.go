98b4e89a8ca751f621b6e164d4743616b73e5e3f
Mikael Berthe <mikael@lilotux.net>
2017-04-06 13:07:50 +0200

    Move Episode structure definition to episodes.go
    Move Episode structure definition and related methods where it makes
    more sense.

--- bsclient/planning.go
+++ bsclient/planning.go
@@ -1,70 +1,9 @@
 package bsclient
 
 import (
-	"errors"
 	"net/url"
 	"strconv"
 )
-
-var (
-	errNoEpisodesFound = errors.New("no episodes found")
-)
-
-// Episode represents the episode data returned by the betaserie API
-type Episode struct {
-	ID        int    `json:"id"`
-	ThetvdbID int    `json:"thetvdb_id"`
-	YoutubeID string `json:"youtube_id"`
-	Title     string `json:"title"`
-	Season    int    `json:"season"`
-	Episode   int    `json:"episode"`
-	Show      struct {
-		ID        int    `json:"id"`
-		ThetvdbID int    `json:"thetvdb_id"`
-		Title     string `json:"title"`
-	} `json:"show"`
-	Code        string `json:"code"`
-	Global      int    `json:"global"`
-	Special     int    `json:"special"`
-	Description string `json:"description"`
-	Date        string `json:"date"`
-	Note        struct {
-		Total int     `json:"total"`
-		Mean  float32 `json:"mean"`
-		User  int     `json:"user"`
-	} `json:"note"`
-	User struct {
-		Seen       bool `json:"seen"`
-		Downloaded bool `json:"downloaded"`
-	} `json:"user"`
-	Comments  string     `json:"comments"`
-	Subtitles []Subtitle `json:"subtitles"`
-}
-
-type episodes struct {
-	Episodes []Episode     `json:"episodes"`
-	Errors   []interface{} `json:"errors"`
-}
-
-func (bs *BetaSeries) doGetEpisodes(u *url.URL, usedAPI string) ([]Episode, error) {
-	resp, err := bs.do("GET", u)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	data := &episodes{}
-	err = bs.decode(data, resp, usedAPI, u.RawQuery)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(data.Episodes) < 1 {
-		return nil, errNoEpisodesFound
-	}
-
-	return data.Episodes, nil
-}
 
 // PlanningGeneral returns a slice of episodes found in [date-before, date+after] timeline.
 // Note: the 'date' input must be in YYYY-MM-JJ format or 'now'
